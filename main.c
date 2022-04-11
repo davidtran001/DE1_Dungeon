aea3bb586ec7ea2099b85ce9022d5a387b11a5bc
Yilin <guoyilin01@gmail.com>
2022-04-10 22:54:04 -0400

    Update main.c

--- main.c
+++ main.c
@@ -371,8 +371,9 @@ void draw_barrell(int x, int y){
 	draw_box (x,y,barrell_color);
 }
 void draw_healthBar(int x, int y, short int color){
-    int i, j;
-        if(p->health ==100){
+    int i, j;      
+             if(p->health == 100){
+             
              for (i = 0; i < 50; i++) {
                  for (j = 0; j <= 6; j++) {
                      plot_pixel(x-i, y-j, green);
@@ -381,9 +382,8 @@ void draw_healthBar(int x, int y, short int color){
                      plot_pixel(x+i, y+j, green);
                  }
              }
-        }
 }
-void calculate_healthBar(struct health *h, struct player play *p ){
+void calculate_healthBar(struct health *h, struct player *p ){
     if((h->x- 50>= 0) && (h->x+ 50<= X_BOUND) && (h->y-6 >= 0) && h->y+6 <= Y_BOUND){
         int i, j;
         if(p->health ==100){
